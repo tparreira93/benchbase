4e9a628707271a63e0977fe0220b9b8dec0173c6
Thales <thalesvparreira@gmail.com>
2022-05-08 16:56:04 +0100

    improves benchmark scripts generation

--- scripts/build_test_configs.py
+++ scripts/build_test_configs.py
@@ -1,18 +1,9 @@
 #!/bin/python3
 
 import os
-import sys
 from pathlib import Path
 from os import listdir
-
-
-def fix_name(string):
-    if string == "lsd":
-        return "jdbc:lsd.v2:postgresql"
-    elif string == "base":
-        return "jdbc:postgresql"
-    else:
-        return string
+import argparse
 
 
 def build_folder(output, benchmark, driver):
@@ -24,7 +15,20 @@ def build_folder(output, benchmark, driver):
 
 
 def build_file_name(benchmark, driver, scale, terminals):
+    if driver.__contains__("lsd"):
+        driver = "lsd"
+    else:
+        driver = "base"
+
     return benchmark + "_d" + driver + "_s" + scale + "_t" + terminals + ".xml"
+
+
+def find_index(data, value):
+    for i in range(len(data)):
+        if value.lower() == data[i].lower():
+            return i
+
+    raise Exception("Missing parameter " + value)
 
 
 def create_test_configs(config_file_path: str, template_file_path: str, output: str):
@@ -37,14 +41,14 @@ def create_test_configs(config_file_path: str, template_file_path: str, output: 
         config_lines = [l.strip().split(",") for l in config_lines[1:] if not l.startswith("#")]
 
         for values in config_lines:
-            benchmark = values[0]
-            driver = values[1]
-            scale = values[2]
-            terminals = values[3]
+            benchmark = values[find_index(template_keys, "benchmark")]
+            driver = values[find_index(template_keys, "driver")]
+            scale = values[find_index(template_keys, "scale")]
+            terminals = values[find_index(template_keys, "terminals")]
             template = template_config
             keys = template_keys[1:]
             for (k, v) in zip(keys, values[1:]):
-                template = template.replace("$" + k, fix_name(v))
+                template = template.replace("$" + k, v)
 
             file_name = build_file_name(benchmark, driver, scale, terminals)
             output_folder = build_folder(output, benchmark, driver)
@@ -66,18 +70,19 @@ def generate_commands(config_dirs):
         base_dir = p.parent.name
         driver = p.name
 
-        args = " --create=true --load=true --execute=true "
+        cmd_args = " --create=true --load=true --execute=true "
         if driver != "base":
-            args += "-b " + base_dir + "_" + driver
+            cmd_args += "-b " + base_dir + "_" + driver
         else:
-            args += "-b " + base_dir
+            cmd_args += "-b " + base_dir
 
-        for f in listdir(dir_name):
+        files = [os.path.join(dir_name, f) for f in listdir(dir_name) if os.path.isfile(os.path.join(dir_name, f))]
+
+        for f in files:
             c = Path(f)
 
-            command = "-c " + str(c.absolute()) + " -d " + str(c.stem) + args
+            command = "-c " + str(c.absolute()) + " -d " + os.path.join(c.parent.absolute(), str(c.stem)) + cmd_args
             commands.append(command)
-
 
     return commands
 
@@ -98,10 +103,11 @@ def aggregate_results(config_dirs):
     return agg.values()
 
 
-def run():
-    config = sys.argv[1]
-    template = sys.argv[2]
-    output = sys.argv[3]
+def run(args):
+    config = args.config
+    template = args.template
+    output = os.path.abspath(args.output)
+    java = os.path.abspath(args.jar)
 
     config_dirs = create_test_configs(config, template, output)
 
@@ -110,16 +116,44 @@ def run():
     with open("run.sh", "w+") as f:
         f.writelines(["#!/usr/bin/env bash", os.linesep])
 
-        for command in commands:
+        f.writelines(["pushd " + java, os.linesep])
 
+        for command in commands:
             f.writelines(["java -jar benchbase.jar " + command, os.linesep])
+
+        f.writelines(["popd", os.linesep])
 
         plots = aggregate_results(config_dirs)
 
         for p in plots:
-            f.writelines(["python plot_data.py " + " ".join(p), os.linesep])
+            f.writelines(["python3 " + os.path.abspath("plot_data.py") + " " + " ".join(p), os.linesep])
 
 
 if __name__ == '__main__':
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        dest="config",
+        help="Configuration CSV with benchmark tests to generate"
+    )
+    parser.add_argument(
+        "-t",
+        "--template",
+        dest="template",
+        help="Benchmark template"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Output directory of test results"
+    )
+    parser.add_argument(
+        "-j",
+        "--jar-path",
+        dest="jar",
+        help="Path to benchmark executable"
+    )
 
+    args = parser.parse_args()
+    run(args)
