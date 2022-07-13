5d0a995dde9fc818424a6bfba14fb6edd5de7573
Thales <thalesvparreira@gmail.com>
2022-07-13 21:30:35 +0100

    fix: test files discovery

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -18,10 +18,11 @@ def rename(df):
     }, inplace=True)
 
 
-def find_plotable_data(csv_file):
+def find_plotable_data(csv_pattern):
     files = []
     for arg in sys.argv[1:-1]:
-        for f in glob.glob("*." + csv_file + ".csv", root_dir=arg):
+        csv_files = [f for f in os.listdir(arg) if f.endswith(csv_pattern + ".csv")]
+        for f in csv_files:
             p = Path(os.path.join(arg, f))
             files.append((str(p.parent.parent.name), str(p)))
 
@@ -52,27 +53,27 @@ def run():
     files = find_plotable_data("results")
     output_path = sys.argv[-1]
 
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
 
     outfile_name = os.path.join(output_path,
                                 "Requests (5 sec samples)" + "_" + datetime.now().strftime("%Y%m%d_%Hh%Mm") + '.png')
     title = "Requests (5 sec)"
     create_plot("Requests (sec)", files, title, ax=axes[0, 0])
 
-    outfile_name = os.path.join(output_path, "Avg. Latency" + "_" + datetime.now().strftime("%Y%m%d_%Hh%Mm") + '.png')
-    title = "Avg Latency"
-    create_plot("Avg. Latency (ms)", files, title, ax=axes[1, 0])
+    # outfile_name = os.path.join(output_path, "Avg. Latency" + "_" + datetime.now().strftime("%Y%m%d_%Hh%Mm") + '.png')
+    # title = "Avg Latency"
+    # create_plot("Avg. Latency (ms)", files, title, ax=axes[1, 0])
 
     outfile_name = os.path.join(output_path, "P99 Latency" + "_" + datetime.now().strftime("%Y%m%d_%Hh%Mm") + '.png')
     title = "P99 Latency"
-    create_plot("P99 Latency (ms)", files, title, ax=axes[1, 1])
+    create_plot("P99 Latency (ms)", files, title, ax=axes[0, 1])
 
     files = find_plotable_data("samples")
 
-    outfile_name = os.path.join(output_path,
-                                "Requests (1 sec sample)" + "_" + datetime.now().strftime("%Y%m%d_%Hh%Mm") + '.png')
-    title = "Requests (1 sec)"
-    create_plot("Requests (sec)", files, title, ax=axes[0, 1])
+    # outfile_name = os.path.join(output_path,
+    #                             "Requests (1 sec sample)" + "_" + datetime.now().strftime("%Y%m%d_%Hh%Mm") + '.png')
+    # title = "Requests (1 sec)"
+    # create_plot("Requests (sec)", files, title, ax=axes[0, 1])
 
     fig.tight_layout(pad=2.0)
     outfile_name = os.path.join(output_path, str(Path(files[0][1]).parent.name) + "_" + datetime.now().strftime(
